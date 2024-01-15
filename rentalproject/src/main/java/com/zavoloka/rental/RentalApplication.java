fffdea29742b835cb5f45d0f8813cfb912e150d7
userCurrent <usercurrentmail@gmail.com>
2024-01-15 16:13:36 +0200

    Java Spring boot rental project

---
+++ rentalproject/src/main/java/com/zavoloka/rental/RentalApplication.java
@@ -0,0 +1,13 @@
+package com.sum17;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentalApplication.class, args);
+	}
+
+}
